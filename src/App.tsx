f89850abc8d3b60987f2b722b1a65c1b9353688a
Andrew Lee <40058794+andrewlee-29@users.noreply.github.com>
2025-12-04 14:29:31 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Navbar'
-import { Route, Routes } from 'react-router'
 import About from './Pages/About'
 import Home from './Pages/Home'
-import Projects from './Pages/Project'
+import Project from './Pages/Project'
+
 function App() {
 
   return (
@@ -12,8 +13,8 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<Home />} /> 
-          <Route path="/projects" element={<Projects />} />
           <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
         </Routes>
       </main>
     </>
