a1e74d54db9003d5334fc0ed133939374f19a654
Andrew Lee <40058794+andrewlee-29@users.noreply.github.com>
2025-12-04 13:53:05 -0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-  
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/andrewlee-29.github.io/', // 替换为你的仓库名
   plugins: [react(), tailwindcss()],
 })
