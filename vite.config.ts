8f99a89a3a903c41a5a50a6f734bd1a12cb5b955
Andrew Lee <40058794+andrewlee-29@users.noreply.github.com>
2025-12-04 13:40:03 -0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
   
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/andrewlee-29.github.io/',
   plugins: [react(), tailwindcss()],
 })
