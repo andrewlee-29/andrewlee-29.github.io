37c997c72d74881971f49cf79cab95dea5a22a53
Andrew Lee <40058794+andrewlee-29@users.noreply.github.com>
2025-12-04 14:10:50 -0500

    Initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import { BrowserRouter } from 'react-router-dom'
+import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter basename="/andrewlee-29.github.io">
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </React.StrictMode>
 )
