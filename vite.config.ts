53a4fbe02d7ec8e08f1e830f46e85bcc3ec3c62f
Andrew Lee <40058794+andrewlee-29@users.noreply.github.com>
2025-12-04 21:41:08 -0500

    Updates

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/', // 用户页面用根路径
-  plugins: [react(), tailwindcss()],
-})
